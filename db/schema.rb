5cc7364b98cc23807c168709d24d88bb7f2dcef9
Krishnaprasad Varma <krshnaprsad@gmail.com>
2012-12-16 20:53:11 +0530

    modified git ignore and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121213073520) do
+ActiveRecord::Schema.define(:version => 20121213073521) do
+
+  create_table "tweets", :force => true do |t|
+    t.integer  "user_id",                                      :null => false
+    t.string   "message",    :limit => 160,                    :null => false
+    t.boolean  "private",                   :default => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+  end
+
+  add_index "tweets", ["private"], :name => "index_tweets_on_private"
+  add_index "tweets", ["user_id"], :name => "index_tweets_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
